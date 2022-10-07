8162a273443f9027b478c98e454a2a50f16713d4
dung.ht@tinhhoa.asia <dung.ht@tinhhoa.asia>
2022-10-07 14:08:22 +0700

    Add project files.

---
+++ ApplicationPractical/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationPractical.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationPractical.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly JwtAuthenticationManager jwtAuthenticationManager;
+        public LoginController(JwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        
+        
+        [AllowAnonymous]
+        [ActionName("Authorize")]
+        [HttpPost]
+        public IActionResult Authorize([FromBody] User usr)
+        {
+            var token = jwtAuthenticationManager.Authenticate(usr.Username, usr.Password);
+            if (string.IsNullOrEmpty(token))
+                return Unauthorized();
+            return Ok(token);
+        }
+
+        [HttpGet]
+        public IActionResult TestRoute()
+        {
+            return Ok("Authorized");
+        }
+    }
+}
